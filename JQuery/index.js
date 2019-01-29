ba641a7b42863104236ada453552972ee0fc92ab
mikaelbeat <ryynanenphm@gmail.com>
2019-01-29 16:00:38 +0200

    Update

--- JQuery/index.js
+++ JQuery/index.js
@@ -30,5 +30,40 @@ $(document).ready(function(){
   // Logs to console given element all classes
   console.log($("h1").attr("class"));
 
+  // Adding click listener
+  $("h1").click(function(){
+    $("h1").css("color", "purple");
+    $("h2").show();
+  })
+
+  $("button").click(function(){
+    $("h1").css("color", "green");
+  })
+
+  $("input").keypress(function(event){
+    console.log(event.key);
+  })
+
+  $("input").keypress(function(event){
+    $("h1").text(event.key);
+  })
+
+  $("h1").on("mouseover", function(){
+    $("h1").css("color", "orange");
+  })
+
+  // Adding new HTML element using JQuery
+    // prepend -> Adds element just before selected element
+    // append -> Adds element just after selected element
+  $("h1").before("<button>New</button>");
+
+  // Remove all button elements
+  // $("button").remove();
+
+  $("button").on("click", function(){
+    $("h2").fadeToggle();
+    // $("h2").on("click", function(){
+    //  $("h1").animate({opacity: 0.5});
+  })
 
 });
