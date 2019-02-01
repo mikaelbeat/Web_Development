01864a0f31240645cc270cc74365a2dc7f0fb248
mikaelbeat <ryynanenphm@gmail.com>
2019-02-01 10:53:49 +0200

    Update

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,24 +1,55 @@
 
-var randomNumber = 0;
 var buttonColours = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
-var userCLickedPattern = [];
 
-function nextSequence(){
-  randomNumber = Math.floor(Math.random() * 4);
+var gamePattern = [];
+var userClickedPattern = [];
+
+var level = 0;
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+});
+
+function nextSequence() {
+
+  level++;
+
+  $("h1").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
 }
 
-nextSequence();
-var randomChosenColour = buttonColours[randomNumber];
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
-var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-audio.play();
+function animatePress(currentColor) {
 
-$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + currentColor).addClass("pressed");
 
-$("button").click(function(){
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
 
+$(document).keypress(function(event){
+  console.log(event.key);
+  nextSequence();
 })
 
-console.log(randomNumber);
-console.log(randomChosenColour);
+function checkAnswer(currentLevel){
+
+}
