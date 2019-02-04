9a0d16c288fa37e94a602945c73448bc824909f8
mikaelbeat <ryynanenphm@gmail.com>
2019-02-04 16:19:35 +0200

    Node-Express update

--- Node-Express/server.js
+++ Node-Express/server.js
@@ -4,6 +4,25 @@
 const express = require("express");
 const app = express();
 
+let request = "This is my request for server!";
+
+app.get("/", function(req, res){
+  res.send("<h1>This is server response!</h1>");
+});
+
+app.get("/contact", function(req, res){
+  res.send("You can contact me at email@gmail.com");
+});
+
+app.get("/about", function(req, res){
+  res.send("This page is owned by MikaelBeat.");
+});
+
+app.get("/hobbies", function(req, res){
+  res.send("<h1>Hobbies</h1><ul><li>Code</li><li>Beer tasting</li>")
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000.")
+  console.log("Request: " + request)
 });
