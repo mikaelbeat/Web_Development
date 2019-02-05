f6d84e1a084dff26252e06cb91dc2d6ac5dcfd0e
mikaelbeat <ryynanenphm@gmail.com>
2019-02-05 11:16:42 +0200

    Update

--- Node-Calculator/calculator.js
+++ Node-Calculator/calculator.js
@@ -7,19 +7,24 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Calculator Section
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res){
-
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
-
-res.send("The result of calculation is " + result);
+  res.send("The result of calculation is " + result);
 
 });
+
+// Calculator Section
+
+
+// BMI Calculator Section
 
 app.get("/bmicalculator", function(req, res){
   res.sendFile(__dirname + "/bmiCalculator.html");
@@ -29,10 +34,11 @@ app.post("/bmicalculator", function(req, res){
   var weight = parseFloat(req.body.weight);
   var height = parseFloat(req.body.height);
   var bmi = parseFloat(weight / (height * height));
+  res.send("For given weight " + weight + " and " + height + " BMI is " + bmi + ".");
 
-  res.send("For given weight " + weight + " and " + height + " BMI is " + bmi + ".")
+});
 
-})
+// BMI Calculator Section
 
 app.listen(3000, function(){
   console.log("Starting calculator server at port 3000.");
