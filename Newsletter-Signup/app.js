1875dab2729c4be04fc3f3d6940e2dfb180c6696
mikaelbeat <ryynanenphm@gmail.com>
2019-02-11 14:26:03 +0200

    Update

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -23,8 +23,45 @@ app.post("/", function(req, res){
   var lastName = req.body.lastName;
   var emailAddress = req.body.emailAddress;
 
+  var data = {
+    members: [
+      {email_address: emailAddress,
+      status: "subscribed",
+    merge_fields: {
+      FNAME: firstName,
+      LNAME: lastName
+    }}
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us20.api.mailchimp.com/3.0/lists/b7d7d5afc1",
+    method: "POST",
+    headers: {
+      "Authorization": "MikaelBeat 55cdc45405226b91749bb62da88e8c6e-us20"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body){
+    if (error){
+      console.log("Error:  " + error);
+    } else {
+      console.log("Status code: " + response.statusCode);
+    }
+  });
+
   console.log("First name: " + firstName);
   console.log("Last name: " + lastName);
   console.log("Email address: " + emailAddress);
 
 });
+
+
+
+
+
+// 55cdc45405226b91749bb62da88e8c6e-us20
+// b7d7d5afc1
