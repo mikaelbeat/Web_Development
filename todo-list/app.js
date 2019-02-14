cec38352a54e7e6c65f5836ca064802a22835dbb
mikaelbeat <ryynanenphm@gmail.com>
2019-02-14 16:02:52 +0200

    Update

--- todo-list/app.js
+++ todo-list/app.js
@@ -2,11 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -18,22 +19,14 @@ app.listen(3000, function(){
 
 app.get("/", function(req, res){
 
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDay();
   res.render("list", {listTitle: day, newItems: items});
 
 });
 
 app.post("", function(req, res){
 
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work"){
     workItems.push(item);
