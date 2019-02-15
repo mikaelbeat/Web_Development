6e8f192a0f5b3fc2a24519b596c658b1d824d733
mikaelbeat <ryynanenphm@gmail.com>
2019-02-15 15:34:22 +0200

    update

--- Ejs Challenge/app.js
+++ Ejs Challenge/app.js
@@ -10,23 +10,39 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+  res.render("home", {welcomeText: homeStartingContent, pMessage: posts});
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutText: aboutContent});
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactText: contactContent});
+});
 
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postText
+  };
 
+  posts.push(post);
+  res.redirect("/");
 
-
-
-
-
-
-
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
