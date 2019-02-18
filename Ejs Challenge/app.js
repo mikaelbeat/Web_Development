ed95e49183ce91c8809e1d4ed1575d96dc6966b7
mikaelbeat <ryynanenphm@gmail.com>
2019-02-18 16:09:02 +0200

    Update

--- Ejs Challenge/app.js
+++ Ejs Challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -21,13 +22,14 @@ app.get("/", function(req, res){
   res.render("home", {welcomeText: homeStartingContent, pMessages: posts});
 });
 
+
 app.get("/posts/:topic", function(req, res){
 
-  const topic = req.params.topic;
+  const topic = lodash.lowerCase(req.params.topic);
+  console.log(topic);
 
   posts.forEach(function(post){
-    const storedTitle = post.title;
-    console.log(storedTitle);
+    const storedTitle = lodash.lowerCase(post.title);
     if (topic === storedTitle){
       console.log("Match found");
     } else {
@@ -35,21 +37,26 @@ app.get("/posts/:topic", function(req, res){
     }
 
   });
-
+//  TÄÄ ON KESKEN
+  res.render("topic", {dHeader: dHeader, dContent: dContent});
 
 });
+
 
 app.get("/about", function(req, res){
   res.render("about", {aboutText: aboutContent});
 });
 
+
 app.get("/contact", function(req, res){
   res.render("contact", {contactText: contactContent});
 });
 
+
 app.get("/compose", function(req, res){
   res.render("compose");
 });
+
 
 app.post("/compose", function(req, res){
   const post = {
@@ -61,6 +68,7 @@ app.post("/compose", function(req, res){
   res.redirect("/");
 
 });
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
