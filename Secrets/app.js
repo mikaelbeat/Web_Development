88b3eb40f17789201a750551c043b5a3d3e144b1
Ryynänen <petri.ryynanen@accenture.com>
2019-03-06 14:55:07 +0200

    Update

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,8 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
+// For hash encryption
+//const md5 = require("md5");
+// For basic encryption
 // const encrypt = require("mongoose-encryption");
 
 const app = express();
@@ -45,33 +49,43 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-    newUser.save(function(err){
-      if (!err){
-        res.render("secrets");
-      } else {
-        console.log("Error: " + err);
-      }
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
     });
+      newUser.save(function(err){
+        if (!err){
+          res.render("secrets");
+        } else {
+          console.log("Error: " + err);
+        }
+      });
+  });
+
+
+
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if (err){
       console.log("Error: " + err);
     } else {
       if (foundUser){
-        if (foundUser.password === password){
-          res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if (result === true){
+            res.render("secrets");
+          }
+        });
+
         }
       }
     }
-  });
+  );
 
 });
